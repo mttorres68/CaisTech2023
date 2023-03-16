47fd6c93bf651b89168f6918daf800172a73cebb
Mateus Miranda Torres <mateusmmt00@gmail.com>
2023-03-16 20:17:55 -0300

    add commit

---
+++ src/Components/link.jsx
@@ -0,0 +1,13 @@
+export default function Link(link){
+
+    return(
+        <>
+            <a
+                className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                href={link}
+            >
+                
+            </a>
+        </>
+    )
+}
