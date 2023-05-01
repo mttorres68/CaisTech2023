41c7009021c38b735c1441160838585e6e0605c8
Mateus Miranda Torres <mateusmmt00@gmail.com>
2023-05-01 18:11:30 -0300

    update caistech2023

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -1,12 +1,12 @@
-const count = new Date("july 04, 2022, 18:00:00").getTime();
-var x = setInterval(function () {
-  var now = new Date().getTime();
-  var d = count - now;
+const count = new Date("may 31, 2023, 18:00:00").getTime();
+let x = setInterval(function () {
+  let now = new Date().getTime();
+  let d = count - now;
 
-  var days = Math.floor(d / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((d % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((d % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((d % (1000 * 60)) / 1000);
+  let days = Math.floor(d / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((d % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((d % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((d % (1000 * 60)) / 1000);
 
   document.getElementById("days").innerHTML = days;
   document.getElementById("hours").innerHTML = hours;
