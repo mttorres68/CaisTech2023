3ff91adb1f52de0f357865f90705a6922452ad74
Mateus Miranda Torres <mateusmmt00@gmail.com>
2023-05-10 15:41:05 -0300

    add_scrollreveal_progrms

---
+++ src/js/scrooll.js
@@ -0,0 +1,67 @@
+window.rs = ScrollReveal({ reset: true });
+
+rs.reveal('.title-programs span', {    
+    duration: 1000,
+    easing: 'cubic-bezier(0.5, 0, 0, 1)' 
+})
+
+rs.reveal('.title-programs h2', {    
+    duration: 1000,
+    easing: 'cubic-bezier(0.5, 0, 0, 1)' 
+})
+rs.reveal('.tabs', {    
+    duration: 1000,
+    easing: 'cubic-bezier(0.5, 0, 0, 1)' 
+})
+rs.reveal('.content-tabs', {    
+    duration: 1000,
+    easing: 'cubic-bezier(0.5, 0, 0, 1)' 
+})
+
+// SPEAKERS
+// rs.reveal('.content-speaker', {
+//     duration: 1000,
+//     easing: 'cubic-bezier(0.5, 0, 0, 1)',
+//     delay: 250
+// })
+
+// rs.reveal('.title-speaker span', {
+//     duration: 1000,
+//     easing: 'cubic-bezier(0.5, 0, 0, 1)' ,
+//     delay: 250
+// })
+
+// rs.reveal('.title-speaker h2', {
+//     duration: 1000,
+//     easing: 'cubic-bezier(0.5, 0, 0, 1)' ,
+//     delay: 250
+// })
+
+// rs.reveal('.info-speaker h2', {
+//     duration: 1000,
+//     easing: 'cubic-bezier(0.5, 0, 0, 1)' ,
+//     delay: 250
+// })
+
+// rs.reveal('.info-speaker .bio-speaker span', {
+//     duration: 1000,
+//     easing: 'cubic-bezier(0.5, 0, 0, 1)' 
+// })
+
+// rs.reveal('.info-speaker .bio-speaker p', {
+//     duration: 1000,
+//     easing: 'cubic-bezier(0.5, 0, 0, 1)' ,
+//     delay: 250
+// })
+
+// rs.reveal('.bio-speaker .speaker-links', {
+//     duration: 1000,
+//     easing: 'cubic-bezier(0.5, 0, 0, 1)',
+//     delay: 250
+// })
+
+// rs.reveal('.speaker-img img', {
+//     duration: 1000,
+//     easing: 'cubic-bezier(0.5, 0, 0, 1)',
+//     delay: 250
+// })
